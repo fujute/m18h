2b2ffcbd6e8221267c1d75ea661c5466be1a79d7
fujute <fuju@narisa.com>
2019-04-17 17:57:26 +0700

    Update azcli-vm-e.sh

--- azcli-vm-e.sh
+++ azcli-vm-e.sh
@@ -7,3 +7,7 @@ az vm start --name $VMName --resource-group $RG
 #az vm stop --name $VMName --resource-group $RG
 #az vm deallocate --name $VMName --resource-group $RG
 
+
+# creating vm with 100gb OS disk
+#az vm list-sizes --location southeastasia --output table  | grep D2s_v3
+#az vm create -n apr17m -g 1RG --image UbuntuLTS --location "southeastasia" --generate-ssh-keys --os-disk-size-gb 100 --size Standard_D2s_v3 --admin-username helloadmin 
