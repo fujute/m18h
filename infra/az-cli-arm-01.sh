61c74f096b8548df8260bbf59f3aaad2b6396b85
fujute <fuju@narisa.com>
2019-06-19 15:00:24 +0700

    Create az-cli-arm-01.sh

---
+++ infra/az-cli-arm-01.sh
@@ -0,0 +1,21 @@
+# ref: https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-template-deploy-cli
+# appName,environment,locationShort,databaseName,administratorLogin,administratorLoginPassword
+# $app_name,dev,dev,appdb6<databasename>,dbuser,<<password>>
+
+resource_group_name="1D614wRG2"
+deployment_name="D614WDeployment"
+app_name="d61webapp"
+
+
+az login
+az group create --name "$resource_group_name"  --location southeastasia
+
+
+az group deployment validate --parameters '{ \
+   "appName": {"value":"$app_name"}, \ 
+   "environment": {"value":"dev"}, \
+   "locationShort": {"value":"sea"}, \
+   "databaseName": {"value":"appdb6<databasename>"}, \
+   "administratorLogin": {"value":"dbuser"}, \
+   "administratorLoginPassword": {"value":"<<password>>}" \
+                            }' --resource-group "$resource_group_name" --template-file https://raw.githubusercontent.com/mspnp/reference-architectures/master/managed-web-app/basic-web-app/Paas-Basic/Templates/PaaS-Basic.json
