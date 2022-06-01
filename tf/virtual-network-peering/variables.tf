5f751dd3cc9c0e16ee564f83e3d7ac5e89de17db
fujute <7958321+fujute@users.noreply.github.com>
2022-06-01 11:25:56 +0700

    Create variables.tf

---
+++ tf/virtual-network-peering/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "The prefix used for all resources in this example"
+  default = "fx1"
+}
+
+variable "prefix2" {
+  description = "The prefix used for all resources in this example"
+  default = "fx2"
+}
+
+variable "location" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "eastasia"
+}
