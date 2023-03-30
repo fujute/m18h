c4150b22a70e6ca3ca9a62a725a0365a733e3826
fujute <7958321+fujute@users.noreply.github.com>
2023-03-31 00:06:13 +0700

    Create aks-az-001.sh

---
+++ aks/aks-az-001.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# https://learn.microsoft.com/en-us/azure/aks/availability-zones 
+
+RG="1TL-300323-AKSAZ-RG-01"
+AKSNAME="myAKSCluster300301"
+LOCATION="westus2"
+
+az group create --name ${RG} --location eastus2
+
+#fix node
+az aks create \
+    --resource-group ${RG} \
+    --name ${AKSNAME} \
+    --generate-ssh-keys \
+    --node-vm-size standard_d2as_v5 \
+    --load-balancer-sku standard \
+    --node-count 3 \
+    --zones 1 2 3 \
+    --location ${LOCATION}
+
+az aks get-credentials --resource-group ${RG} --name ${AKSNAME}
+
+kubectl describe nodes | grep -e "Name:" -e "topology.kubernetes.io/zone"
+kubectl get nodes -o custom-columns=NAME:'{.metadata.name}',REGION:'{.metadata.labels.topology\.kubernetes\.io/region}',ZONE:'{metadata.labels.topology\.kubernetes\.io/zone}'
+
+az aks scale --resource-group ${RG} --name ${AKSNAME} --node-count 2
+kubectl describe nodes | grep -e "Name:" -e "topology.kubernetes.io/zone"
+
+az aks scale --resource-group ${RG} --name ${AKSNAME} --node-count 3
+kubectl describe nodes | grep -e "Name:" -e "topology.kubernetes.io/zone"
+
+
+## App
+kubectl create deployment nginx --image=mcr.microsoft.com/oss/nginx/nginx:1.15.5-alpine
+kubectl scale deployment nginx --replicas=3
+kubectl describe pod | grep -e "^Name:" -e "^Node:"
+## 
+
+
+az group delete --name ${RG} 
+az group delete --name 1TL-300323-AKSAZ-RG
+
+## Second Cluster 
+RG="1TL-300323-AKSAZ-RG-01"
+AKSNAME="myAKSCluster300302"
+LOCATION="westus2"
+
+#autoscale
+az aks create \
+    --resource-group ${RG} \
+    --name ${AKSNAME} \
+    --generate-ssh-keys \
+    --vm-set-type VirtualMachineScaleSets \
+    --node-vm-size standard_d2as_v5 \
+    --enable-cluster-autoscaler --min-count 3 --max-count 6 \
+    --load-balancer-sku standard \
+    --node-count 3 \
+    --zones 1 2 3 \
+    --location ${LOCATION}
+
+az aks get-credentials --resource-group ${RG} --name ${AKSNAME}
+
+kubectl describe nodes | grep -e "Name:" -e "topology.kubernetes.io/zone"
+kubectl get nodes -o custom-columns=NAME:'{.metadata.name}',REGION:'{.metadata.labels.topology\.kubernetes\.io/region}',ZONE:'{metadata.labels.topology\.kubernetes\.io/zone}'
+
+# https://learn.microsoft.com/en-us/azure/aks/cluster-autoscaler
+az aks update --resource-group ${RG} --name ${AKSNAME} \
+  --update-cluster-autoscaler --min-count 2 --max-count 6
+
+kubectl get nodes -o custom-columns=NAME:'{.metadata.name}',REGION:'{.metadata.labels.topology\.kubernetes\.io/region}',ZONE:'{metadata.labels.topology\.kubernetes\.io/zone}'
+
+az aks update --resource-group ${RG} --name ${AKSNAME} \
+--update-cluster-autoscaler --min-count 3 --max-count 6
+
+kubectl get nodes -o custom-columns=NAME:'{.metadata.name}',REGION:'{.metadata.labels.topology\.kubernetes\.io/region}',ZONE:'{metadata.labels.topology\.kubernetes\.io/zone}'
